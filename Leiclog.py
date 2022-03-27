9d6a7907e6c0bf46869b35c50640f6bb243ce352
panos1998 <petridisp@ece.auth.gr>
2022-03-28 00:06:45 +0300

    Leicester Logistic fullfiled

--- Leiclog.py
+++ Leiclog.py
@@ -1,24 +1,32 @@
 #%%
 from arfftocsv import arfftocsv, labelize, dataEncoding
-import pandas as pd
 import numpy as np
+import pandas as pd
 import tqdm
-all_labels = ['LeicGender','LeicRace','raeducl','mstat','shlt','hlthlm','mobilb','lgmusa','grossa','finea','LeicHBP','LeicAge',
-'hearte','psyche','bmicat','physActive','drinkd_e','smoken','itot','cfoodo1m','jphysa','estwt','wstva','chol','hdl','ldl',
-'trig','sys1','dias3','fglu','hba1c','hemda','eatVegFru','everHighGlu','rYdiabe']
+all_labels = ['LeicGender','LeicRace','raeducl','mstat','shlt','hlthlm',
+'mobilb','lgmusa','grossa','finea','LeicHBP','LeicAge','hearte',
+'psyche','bmicat','physActive','drinkd_e','smoken','itot','cfoodo1m',
+'jphysa','estwt','wstva','chol','hdl','ldl','trig','sys1','dias3',
+'fglu','hba1c','hemda','eatVegFru','everHighGlu','rYdiabe']
 
-labels = ['LeicAge','LeicGender','bmicat','LeicRace','hemda','rYdiabe']
+labels = ['LeicAge','LeicGender','bmicat','LeicRace','hemda','wstva','LeicHBP','rYdiabe']
 
 to_replace = {'LeicAge': ['50-59', '60-69', '>=70'], 'LeicGender': ['Female', 'Male'], 
-'bmicat': ["'1.underweight less than 18.5'", "'2.normal weight from 18.5 to 24.9'", "'3.pre-obesity from 25 to 29.9'",
-"'4.obesity class 1 from 30 to 34.9'", "'5.obesity class 2 from 35 to 39.9'", "'6.obesity class 3 greater than 40'"],
-'LeicRace': [0, 6], 'hemda': ["'Not applicable'", 'Yes', 'No'], 'rYdiabe': ['0.no', '1.yes']
+'bmicat': ["'1.underweight less than 18.5'",
+ "'2.normal weight from 18.5 to 24.9'", "'3.pre-obesity from 25 to 29.9'",
+ "'4.obesity class 1 from 30 to 34.9'", "'5.obesity class 2 from 35 to 39.9'",
+  "'6.obesity class 3 greater than 40'"],
+'LeicRace': [0, 6], 'hemda': ["'Not applicable'", 'Yes', 'No'],
+ 'wstva':[],'LeicHBP': ['No', 'Yes'], 'rYdiabe': ['0.no', '1.yes']
 }
 
-values = {'LeicAge': [0, 1, 2], 'LeicGender': [0, 1], 'bmicat': [1, 2, 3, 4, 5, 6], 'LeicRace': [0, 6],
- 'hemda': [0, 1, 2], 'rYdiabe': [0, 1]}
+values = {'LeicAge': [0, 1, 2], 'LeicGender': [0, 1],
+ 'bmicat': [1, 2, 3, 4, 5, 6],'LeicRace': [0, 6],
+ 'hemda': [0, 1, 2], 'wstva':[],'LeicHBP':[0,1],'rYdiabe': [0, 1]}
 # this function places the labels for each model and converts categorical to numerical data
-def processing (all_labels: list, labels: list, to_replace: dict, values: dict, path, source: str = 'diabetes_paper_fazakis.csv',
+def processing (all_labels: list, labels: list,
+ to_replace: dict, values: dict,path,
+  source: str = 'diabetes_paper_fazakis.csv',
  des: str  ='Finaldata.csv')-> pd.DataFrame:
  arfftocsv(source)
  df = labelize(des, all_labels)
@@ -28,11 +36,10 @@ data = processing(all_labels, labels, to_replace, values, path)
 # Apply machine learning techniques
 X = data[labels[:-1]] # get the features
 y = data[labels[len(labels)-1]] # get the target class
-from sklearn.linear_model import LogisticRegression 
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV 
 from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegressionCV
 """
 clf = LogisticRegressionCV(cv=5).fit(X, y)
 y_pred = (clf.predict_proba(X)[:,1]>=0.148).astype(bool)
@@ -47,11 +54,9 @@ aucs = np.zeros((10, 8, 100)) # initialize an array to store aucs
 for k in tqdm.tqdm(range(100), colour='CYAN'): # for 100 epochs
     for i in range(0,10): # run through 10 different stratified datasets
        j = 0                                       # TRUE NEGATIVE RATE = SPECIFICITY
-       X_train, X_test, y_train, y_test = train_test_split(X, y,
-       train_size=  0.7, test_size=0.3, stratify=y) # stratified train/test split 70/30
+       X_train, X_test, y_train, y_test = train_test_split(X, y,train_size= 0.7,test_size=0.3, stratify=y) # stratified train/test split 70/30
        for c in [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]:#evaluate each dataset over 8 different C values
-           clf = LogisticRegression(C=c, solver='liblinear',
-            penalty='l2').fit(X_train, y_train) # train classifier over a dataset for C values
+           clf = LogisticRegression(C=c, solver='liblinear',penalty='l2').fit(X_train, y_train) # train classifier over a dataset for C values
            y_pred = (clf.predict_proba(X_test))[:,1] # get prob predictions
            fpr, sensitivity, thresholds = roc_curve(y_test, y_pred) # get metrics
            aucs[i, j, k] =(roc_auc_score(y_test, y_pred)) #3-order tensor saves auc for each C
@@ -59,19 +64,21 @@ for k in tqdm.tqdm(range(100), colour='CYAN'): # for 100 epochs
 means = np.mean(np.mean(aucs, axis =2), axis =0) # mean auc per c over all datasets and epochs
 print(means)
 plt1 = plt.figure(1)
+plt.xlabel('C value')
+plt.ylabel('Mean AUC')
 plt.plot(['0.0001', '0.001', '0.01', '0.1', '1', '10', '100', '1000'], means)
 # να δουμε αυριο τι να αλλαξουμε μπας κ φτασουμε τα σωστα, οπως regularization C, penalty l1, l2 
 #Υπαρχει μια απόκλιση 4%
 #Εδω θα βαλουμε αυριο το κωδικα για να βρουμε το threshold, να ψαξω αν πρωτα πρεπει να κανω  training  και μετα hyper
 # parameters ή βολευει οπως το εκανα
- 
 scores = list() # initialize a list to save scores
-thresholds = np.linspace(0, 1, 1000) # make a thr space to iterate over
+thresholds = np.linspace(0, 1, 100) # make a thr space to iterate over
 for thr in tqdm.tqdm(thresholds): # for different threshold values
     younden = list() # initialize a list to save younden index for every 10-group evaluation
     for i in range(0,10): #for every dataset       # TRUE NEGATIVE RATE = SPECIFICITY
        X_train, X_test, y_train, y_test = train_test_split(X, y, 
-       train_size=  0.7, test_size=0.3, stratify=y) # stratified train test split 70/30
+       train_size=0.7,test_size=0.3,
+        stratify=y) # stratified train test split 70/30
        clf = LogisticRegression(C=100, solver='liblinear', penalty='l2').fit(X_train, y_train)
        y_pred = (clf.predict_proba(X_test)[:,1] >= thr).astype(bool)# predict using current thr
        tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel() # get confusion matrix
@@ -85,6 +92,8 @@ params =list(clf.get_params().values())  # get classifier parameters
 optimal =max(scores, key=lambda score: score[0]) # best metrics over all thresholds
 print('Maximum younden,specificity, sensitivity, threshold, c and penalty ',optimal,params[0], params[9])
 plt2 = plt.figure(2)
+plt.ylabel('Younden index')
+plt.xlabel('Threshold value')
 plt.plot([x[3] for x in scores], [x[0] for x in scores])
 plt.show()
 
@@ -99,7 +108,7 @@ plt.show()
 younden_final = list()
 for i in tqdm.tqdm(range(0,10)):                                      # TRUE NEGATIVE RATE = SPECIFICITY
     X_train, X_test, y_train, y_test = train_test_split(X, y, 
-    train_size=  0.7, test_size=0.3, stratify=y)
+    train_size=0.7,test_size=0.3, stratify=y)
     clf = LogisticRegression(C=100, solver='liblinear', penalty='l2').fit(X_train, y_train)
     y_pred = (clf.predict_proba(X_test)[:,1] >= optimal[3]).astype(bool)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
@@ -116,5 +125,9 @@ ax[0].set_ylabel('Younden Index')
 ax[2].set_ylabel('Sensitivity')
 ax[1].set_ylabel('Specificity')
 plt.show()
-print(np.mean(younden_final, axis=0))
+print('Average Scores: ',np.mean(younden_final, axis=0))
+print('Max Specificity,''Sensitivity: ',
+np.amax(younden_final, axis=0)[1:3])
+print('Min Specificity, Sensitivity: ',
+np.amin(younden_final, axis=0)[1:3])
 # %%
