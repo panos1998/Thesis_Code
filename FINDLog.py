01ebb5895a07727d4de30b341cf2ca0afb273476
panos1998 <petridisp@ece.auth.gr>
2022-04-23 16:48:05 +0300

    readability

--- FINDLog.py
+++ FINDLog.py
@@ -1,9 +1,11 @@
-#%%
-from turtle import width
-from arfftocsv import processing
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import  roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
+from arfftocsv import processing
 import tqdm
-from evaluation import evaluation
+from evaluation import function_evaluation
 all_labels = ['LeicGender','LeicRace','raeducl','mstat','shlt','hlthlm',
 'mobilb','lgmusa','grossa','finea','LeicHBP','LeicAge','hearte',
 'psyche','bmicat','physActive','drinkd_e','smoken','itot','cfoodo1m',
@@ -32,16 +34,10 @@ data = processing(all_labels, labels, to_replace, values, path)
 # Apply machine learning techniques
 X = data[labels[:-1]] # get the features
 y = data[labels[len(labels)-1]] # get the target class
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import  roc_auc_score, roc_curve
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-
 aucs = np.zeros((10, 8, 100)) # initialize an array to store aucs
-#for j in range (500):         # TRUE POSITIVE RATE = SENSITIVITY
 for k in tqdm.tqdm(range(100), colour='CYAN'): # for 100 epochs
     for i in range(0,10): # run through 10 different stratified datasets
-       j = 0                                       # TRUE NEGATIVE RATE = SPECIFICITY
+       j = 0              # TRUE NEGATIVE RATE = SPECIFICITY
        X_train, X_test, y_train, y_test = train_test_split(X, y,
        train_size= 0.7,test_size=0.03, stratify=y) # stratified train/test split 70/30
        for c in [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]:#evaluate each dataset over 8 different C values
@@ -56,11 +52,5 @@ plt1 = plt.figure(1)
 plt.xlabel('C value')
 plt.ylabel('Mean AUC')
 plt.plot(['0.0001', '0.001', '0.01', '0.1', '1', '10', '100', '1000'], means)
-# να δουμε αυριο τι να αλλαξουμε μπας κ φτασουμε τα σωστα, οπως regularization C, penalty l1, l2 
-#Υπαρχει μια απόκλιση 4%
-#Εδω θα βαλουμε αυριο το κωδικα για να βρουμε το threshold, να ψαξω αν πρωτα πρεπει να κανω  training  και μετα hyper
-# parameters ή βολευει οπως το εκανα
-#%%
 clf = LogisticRegression(C=100, solver='liblinear', penalty='l2').fit(X_train, y_train)
-evaluation(clf, X, y)
-# %%
+function_evaluation(clf, X, y)
