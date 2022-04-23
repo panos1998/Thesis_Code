c2aeb8cca2121525a42f983a518f9ea0d4e51a53
panos1998 <petridisp@ece.auth.gr>
2022-04-23 15:30:37 +0300

    readability

--- evaluation.py
+++ evaluation.py
@@ -1,18 +1,22 @@
+import numpy as np
+"""import sklearn modules for train/test spliting and scoring"""
 from sklearn.metrics import  roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split
-import numpy as np
-def evaluation(clf,  X, y,n_iterations: int=10):
+def function_evaluation(clf,  X, y,n_iterations: int=10):
+    """This function evaluates a model
+Input: a classifier, features array X, label vector y,
+ number of iterations n_iterations"""
     aucs = list()
     bests = list()
-    for i in range(0, n_iterations):
+    for _ in range(0, n_iterations):
        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
        train_size=0.7, stratify=y)
        clf.fit(X_train,y_train)
        probs = clf.predict_proba(X_test)[:,1]
-       aucs.append(roc_auc_score(y_test, probs))
-       fpr, tpr, thr= roc_curve(y_test, probs)
-       metrics = [(tp,1-fp, th) for tp, fp, th in zip(tpr, fpr, thr)]
-       bests.append(max(metrics, key=lambda tuple: tuple[0]+tuple[1]))
+       aucs.append(roc_auc_score(y_test,probs))
+       fpr,tpr,thr= roc_curve(y_test,probs)
+       metrics = [(tp,1-fp, th) for tp,fp,th in zip(tpr,fpr,thr)]
+       bests.append(max(metrics,key=lambda tuple:tuple[0]+tuple[1]))
    #print(metrics)
     print('best AUC: ',np.max(aucs), 'mean AUC: ', np.mean(aucs),'min AUC: ', np.min(aucs))
     best = max(bests,key=lambda tuple:tuple[0]+tuple[1])
@@ -22,3 +26,4 @@ def evaluation(clf,  X, y,n_iterations: int=10):
     print('J: ', best[0]+best[1]-1,'Threshold: ', best[2],'sensitivity: ',
      best[0],'specificity: ',best[1])
     return clf
+    
