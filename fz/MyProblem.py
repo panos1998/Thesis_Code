b84d98f4a4bd11bf3cd1e6cbe411c39b8e6b7639
panos1998 <petridisp@ece.auth.gr>
2022-04-25 02:37:13 +0300

    reduced weighted_data call

--- fz/MyProblem.py
+++ fz/MyProblem.py
@@ -2,6 +2,7 @@ import numpy as np
 from objective_function import objective_function
 from pymoo.core.problem import ElementwiseProblem
 from weighted_data import weighted_data
+X, y = weighted_data()
 class MyProblem(ElementwiseProblem):
     def __init__(self):
         super().__init__ (n_var=2,
@@ -10,7 +11,7 @@ class MyProblem(ElementwiseProblem):
         xl = np.array([0,0]), #lower bound
         xu= np.array([1,1])) #upper bound
     def _evaluate(self, x, out,*args, **kwargs):
-        f1, f2 = objective_function(weighted_data()[0], weighted_data()[1], weights = x)
+        f1, f2 = objective_function(X, y, weights = x)
         g1 = x[0] + x[1] - 1 # the 2 constraints  for w1, w2
         g2 = -x[0] - x[1] + 0.99
         out["F"] = [f1,f2] # export objectives
