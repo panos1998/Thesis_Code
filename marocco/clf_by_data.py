02bd769f51e1f22fc06efc75bbf0672441f3fcc8
panos1998 <petridisp@ece.auth.gr>
2022-04-27 00:59:33 +0300

    playing with random state

--- marocco/clf_by_data.py
+++ marocco/clf_by_data.py
@@ -1,3 +1,4 @@
+
 from typing import List
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,7 +9,7 @@ y: pd.DataFrame(),labels:List[str]=['mean', 'knn', 'rf','mice'],
 colors:List[str]= ['red', 'yellow', 'green', '#1f77b4']):
   for X, color, label in zip(features, colors, labels):
     X_train, X_test, y_train, y_test = train_test_split(X, y,
-test_size = 0.25, random_state =100)
+test_size = 0.25, random_state =600)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     fpr, tpr, thresholds = roc_curve(y_test, y_pred)
