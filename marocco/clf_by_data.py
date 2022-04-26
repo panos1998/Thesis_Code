d65d85ee4272e4e31214b8637fcc6af593b6cd2f
panos1998 <petridisp@ece.auth.gr>
2022-04-26 21:57:18 +0300

    roc without probabilities

--- marocco/clf_by_data.py
+++ marocco/clf_by_data.py
@@ -8,9 +8,9 @@ y: pd.DataFrame(),labels:List[str]=['mean', 'knn', 'rf','mice'],
 colors:List[str]= ['red', 'yellow', 'green', '#1f77b4']):
   for X, color, label in zip(features, colors, labels):
     X_train, X_test, y_train, y_test = train_test_split(X, y,
-test_size = 0.25, random_state =6)
+test_size = 0.25, random_state =100)
     clf.fit(X_train, y_train)
-    y_pred = clf.predict_proba(X_test)[:,1]
+    y_pred = clf.predict(X_test)
     fpr, tpr, thresholds = roc_curve(y_test, y_pred)
     plt.plot(fpr,tpr, label=label, color =color)
   plt.title(name)
